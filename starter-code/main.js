102008d8ed8794fe67318212c1a50d16d5dbc942
Will Hawkins <Will Hawkins>
2017-01-08 11:14:15 -0500

    added createCards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,20 @@ var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "King";
 
-if (cardTwo == cardFour) {
+/*if (cardTwo == cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.")
-}
+}*/
+
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = "card";
+		gameBoard.appendChild(newCard);
+	};
+};
+createCards();
